a457a3d567e568b5bc1f1580a3fe7efcf0c35fdd
laric526 <laric526@hsd.k12.or.us>
2021-09-19 10:22:12 -0700

    Added drive mode switching

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Hardware {
+    public DcMotor leftDrive;
+    public DcMotor rightDrive;
+
+    public void init(HardwareMap hardwareMap) {
+        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+    }
+}
